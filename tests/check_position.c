002d13711a2573500adc991fe29765cb85a25222
Tom Rives <tom.rives@epitech.eu>
2023-07-23 14:52:07 +0200

    [ADD] Maps

---
+++ tests/check_position.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2019
+** check_postion.c
+** File description:
+** check_position
+*/
+
+#include <criterion/criterion.h>
+#include "my_sokoban.h"
+
+Test (, check_player_position)
